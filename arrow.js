33b0c79d9f286ec7a457b7860270885252bce931
Uetani <satoshi.0411@icloud.com>
2020-11-25 16:25:06 +0900

    arrowFuction thisエラーの解決 ES5

--- arrow.js
+++ arrow.js
@@ -7,3 +7,48 @@ const add = function(a, b) {
 const add = (a, b) => {
   return a + b;
 }
+
+// ES6
+const add = (a, b) => return a + b;
+
+
+
+// -------------------------------------
+// ES5
+const numbers =[1,2,3];
+numbers.map(function(number) {
+  return number * 2;
+});
+
+// ES6
+const numbers =[1,2,3];
+numbers.map(number => number * 2);
+// -------------------------------------
+
+
+const teams = {
+  members: ["太郎", "花子"],
+  teamName: "スーパーチーム",
+  teamSummary: function() {
+    return this.members.map(function(member) {
+      return `${member}は${this.teamName}所属です。`
+    });
+  }
+};
+
+
+
+// ES５の解決方法
+const teams = {
+  members: ["太郎", "花子"],
+  teamName: "スーパーチーム",
+  teamSummary: function() {
+    return this.members.map(function(member) {
+      return `${member}は${this.teamName}所属です。`
+    }.bind(this);
+  }
+};
+
+// thisのエラー解決方法２つ
+// ①.bind(this)を記述する
+// ②var selt = this;を使って、thisの代わりにselfを使用する
