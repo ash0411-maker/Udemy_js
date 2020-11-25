c288efff1247b4170a307f9c370daae705da2aa7
Uetani <satoshi.0411@icloud.com>
2020-11-25 18:06:22 +0900

    関数の引数　分割代入

--- destructing.js
+++ destructing.js
@@ -11,3 +11,32 @@ let amount = expense.amount;
 //-------    ES6（分割代入）  --------------
 
 const { type, amount } = expense;
+
+
+
+
+
+
+
+//-------    ES5  --------------
+let savedFile = {
+  extension: "jpeg",
+  name: "profile",
+  size: 14040
+}
+
+function fileSummary(file) {
+  return `${file.name}.${file.extension}の容量は${file.size}です。`
+}
+
+
+//-------    ES6（分割代入）  --------------
+let savedFile = {
+  extension: "jpeg",
+  name: "profile",
+  size: 14040
+}
+
+function fileSummary({name, extension, size}) {
+  return `${name}.${extension}の容量は${size}です。`
+}
