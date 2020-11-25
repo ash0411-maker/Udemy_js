13267bfe1abbe83bfda43871e04cd4f8b6a73967
Uetani <satoshi.0411@icloud.com>
2020-11-25 13:41:08 +0900

    every UseCase

--- every.js
+++ every.js
@@ -22,3 +22,45 @@ for(let i = 0; i < computers.length; i++) {
 computers.every(function(computer) {
   return computer.ram >= 16;
 })
+
+
+computers.some(function(computer) {
+  return computer.ram >= 16;
+})
+
+
+
+
+
+function Field(value) {
+  this.value = value;
+}
+
+Field.prototype.validate = function() {
+  return this.value.length > 0;
+}
+
+let username = new Field('my_username');
+let password = new Field('my_password');
+let birth = new Field('my_birth');
+
+username.validate() && password.validate() && birth.validate()
+
+
+
+
+var fields = [
+	username,
+	password,
+	birth
+];
+
+let formIsValid = fields.every(function(field) {
+ return field.validate();
+}
+
+if(formIsValid) {
+	サーバーにリクエストを投げる
+} else {
+	エラーを表示させる
+}
