4b0daecf090a3ab8dc4923c9d2737b388df591b1
Uetani <satoshi.0411@icloud.com>
2020-11-25 17:46:38 +0900

    Rest演算子

--- spreadAndRest.js
+++ spreadAndRest.js
@@ -5,5 +5,24 @@ function addNumbers(...numbers) {
     return sum + number;
   }, 0);
 }
-
 addNumbers(1, 2, 3, 4, 5)
+
+
+
+// spread演算子
+const defaultColors = ["赤", "黄"];
+const userFavoriteColors = ["オレンジ", "黄"];
+
+[...defaultColors, ...userFavoriteColors]
+
+
+// 指定された値が最初に現れたインデックスを返します。値が見つからない場合は -1 を返します
+function validateShoppingList(...items) {
+  if(items.indexOf("牛乳") < 0) {
+    return ["牛乳", ...items];
+  }
+
+  return items
+}
+
+validateShoppingList("オレンジ", "パン")
