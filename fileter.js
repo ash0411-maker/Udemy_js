843d947c1ee667f8486c4f96d7a7705fd7ec69a5
Uetani <satoshi.0411@icloud.com>
2020-11-25 11:30:15 +0900

    filter

---
+++ fileter.js
@@ -0,0 +1,28 @@
+let products = [
+  {name: "きゅうり", type: "野菜", quantity: 0, price: 1},
+  {name: "バナナ", type: "フルーツ", quantity: 10, price: 15},
+  {name: "セロリ", type: "野菜", quantity: 30, price: 9},
+  {name: "オレンジ", type: "フルーツ", quantity: 3, price: 5}
+];
+
+let filteredProducts = [];
+
+for(let i = 0; i <= products.length; i++) {
+  if(products[i].type === "フルーツ") {
+    filteredProducts.push(products[i])
+  }
+}
+
+
+products.filter(function(product) {
+  return product.type === "フルーツ"；
+});
+
+
+
+// 種類が野菜で、量が０個より多くて、値段が１０より小さい
+products.filter(function(product) {
+  return product.type === "野菜"
+    && product.quantity > 0
+    && product.price < 10
+});
